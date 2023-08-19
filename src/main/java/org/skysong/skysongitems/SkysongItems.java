6bcecf94304fb5daf3bb6d92a4cb0bf942db499b
Ghiojo <ghiojo@hotmail.com>
2023-08-20 01:25:13 +0200

    Project Start

---
+++ src/main/java/org/skysong/skysongitems/SkysongItems.java
@@ -0,0 +1,16 @@
+package org.skysong.skysongitems;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SkysongItems extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello from Ghiojo");
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
