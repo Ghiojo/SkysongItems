5c8426d7bd488107ab1fc3d5be4924f20f64dd36
Ghiojo <ghiojo@hotmail.com>
2023-08-20 21:29:57 +0200

    Project Start

--- src/main/java/org/skysong/skysongitems/SkysongItems.java
+++ src/main/java/org/skysong/skysongitems/SkysongItems.java
@@ -1,12 +1,19 @@
 package org.skysong.skysongitems;
 
+import com.bergerkiller.bukkit.common.cloud.CloudSimpleHandler;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.skysong.skysongitems.commands.BaseCommands;
 
 public final class SkysongItems extends JavaPlugin {
+
+
+    private CloudSimpleHandler commandHandler = new CloudSimpleHandler();
 
     @Override
     public void onEnable() {
         getLogger().info("Hello from Ghiojo");
+        commandHandler.enable(this);
+        commandHandler.getParser().parse(new BaseCommands(this));
     }
 
     @Override
---
+++ src/main/java/org/skysong/skysongitems/commands/BaseCommands.java
@@ -0,0 +1,15 @@
+package org.skysong.skysongitems.commands;
+
+import org.skysong.skysongitems.SkysongItems;
+
+public class BaseCommands
+{
+        private SkysongItems plugin;
+
+    public BaseCommands(SkysongItems plugin) {
+        this.plugin = plugin;
+    }
+
+
+
+}
